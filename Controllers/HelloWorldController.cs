8034392565fa5787c9e08a5f9713ab30a19ed422
Po Lien Lin <polien.joe.lin@gmail.com>
2024-04-10 10:35:01 +0800

    Add new view and ViewData["title"]

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -7,14 +7,16 @@ public class HelloWorldController : Controller
 {
   // 
   // GET: /HelloWorld/
-  public string Index()
+  public IActionResult Index()
   {
-    return "This is my default action...";
+    return View();
   }
   // 
   // GET: /HelloWorld/Welcome/ 
-  public string Welcome(string name, int ID = 1)
+  public IActionResult Welcome(string name, int numTimes = 1)
   {
-    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    ViewData["Message"] = "Hello " + name;
+    ViewData["NumTimes"] = numTimes;
+    return View();
   }
 }
