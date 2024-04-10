c8b74785626e9da54639cf2f0f43b99fee4cabc1
Po Lien Lin <polien.joe.lin@gmail.com>
2024-04-10 21:19:27 +0800

    Add validation of forms

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,13 +7,28 @@ namespace MvcMovie.Models;
 public class Movie
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(60, MinimumLength = 3)]
     public string? Title { get; set; }
+
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
     public string? Genre { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
     [Display(Name = "Price in USD")]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
     public string? Rating { get; set; }
 }
