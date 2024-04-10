b39a54072a1fcf3d1978c411d1541fcb8488d39e
Po Lien Lin <polien.joe.lin@gmail.com>
2024-04-10 20:48:42 +0800

    Update Database by adding Rating

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -15,4 +15,5 @@ public class Movie
     [Display(Name = "Price in USD")]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+    public string? Rating { get; set; }
 }
--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -25,12 +25,14 @@ public static class SeedData
             Title = "When Harry Met Sally",
             ReleaseDate = DateTime.Parse("1989-2-12"),
             Genre = "Romantic Comedy",
+            Rating = "R",
             Price = 7.99M
           },
           new Movie
           {
             Title = "Ghostbusters ",
             ReleaseDate = DateTime.Parse("1984-3-13"),
+            Rating = "R",
             Genre = "Comedy",
             Price = 8.99M
           },
@@ -38,6 +40,7 @@ public static class SeedData
           {
             Title = "Ghostbusters 2",
             ReleaseDate = DateTime.Parse("1986-2-23"),
+            Rating = "R",
             Genre = "Comedy",
             Price = 9.99M
           },
@@ -45,6 +48,7 @@ public static class SeedData
           {
             Title = "Rio Bravo",
             ReleaseDate = DateTime.Parse("1959-4-15"),
+            Rating = "R",
             Genre = "Western",
             Price = 3.99M
           }
